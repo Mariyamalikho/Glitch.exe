99dd514bbb122dbd4f4beec89537d696a9760472
Mariyamalikho <Mariyam.suk22@gmail.com>
2026-07-28 14:36:59 +0500

    style: add Cyberpunk color constants

---
+++ src/utils/colors.ts
@@ -0,0 +1,11 @@
+/**
+ * Core Cyberpunk Color Constants used across the OS Frame and character portraits.
+ */
+export const CYBERPUNK_COLORS = {
+  cyan: '#22D3EE',
+  pink: '#EC4899',
+  purple: '#A855F7',
+  white: '#ECECEC',
+  red: '#FF4D6D',
+  dark: '#050506'
+};
