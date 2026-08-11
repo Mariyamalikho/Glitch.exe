0cb0a83644f404d2e13f5140527adc9f4305b3cb
Mariyamalikho <Mariyam.suk22@gmail.com>
2026-08-11 20:42:58 +0500

    feat: build DialogueBox component

---
+++ src/components/DialogueBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Component to render character dialogue and choices.
+ */
+export function DialogueBox() {
+  return (
+    <div className="dialogue-box border border-cyan/40 p-4 bg-black/80">
+      <p className="text-cyan">[Dialogue placeholder]</p>
+    </div>
+  );
+}
