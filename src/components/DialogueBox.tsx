6aa9a88c63351f39bfca8d716a65143d6f80bc77
Mariyamalikho <Mariyam.suk22@gmail.com>
2026-08-16 17:50:08 +0500

    feat: add Character portraits to dialogue box

--- src/components/DialogueBox.tsx
+++ src/components/DialogueBox.tsx
@@ -7,8 +7,12 @@ export function DialogueBox() {
   // TODO: Add logic to handle user dialogue choices and prevent rapid clicking
 
   return (
-    <div className="dialogue-box border border-cyan/40 p-4 bg-black/80">
-      <p className="text-cyan">[Dialogue placeholder]</p>
+    <div className="dialogue-box border border-cyan/40 p-4 bg-black/80 flex gap-4 items-start">
+      {/* Portrait rendered here */}
+      <div className="portrait w-16 h-16 bg-cyan/20 border border-cyan/50"></div>
+      <div className="text-content">
+        <p className="text-cyan">[Dialogue placeholder]</p>
+      </div>
     </div>
   );
 }
